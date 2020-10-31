413c2317e8a0dcd3e47c43b2922635c0be0a4bfc
zhanghongjie <zhanghongjie@konka.com>
2020-10-31 15:16:45 +0800

    根据意见修改作业

--- week01/BackSpaceStringCompare.java
+++ week01/BackSpaceStringCompare.java
@@ -1,15 +1,47 @@
 public class BackSpaceStringCompare {
     public int solution(int n) {
-        //动态规划
+        //采用双指针 + 倒叙遍历的方法 ，空间复杂度为O(1)
+        int i = S.length() - 1;
+        int j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+        while (i >= 0 || j >= 0) {
 
-        int first = 0;
-        int second = 1;
-        int third = 0;
-        for (int i = 1; i <= n; i++) {
-            third = first + second;
-            first = second;
-            second = third;
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            if (i >= 0 && j >= 0) {
+                if (S.charAt(i) != T.charAt(j)) {
+                    return false;
+                }
+            } else {
+                if (i >= 0 || j >= 0) {
+                    return false;
+                }
+            }
+            i--;
+            j--;
         }
-        return third;
+        return true;
     }
 }
